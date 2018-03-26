8b75602b037f349f180c5e17faef919bfa63d9bd
Romain Gille <romain.gille@gmail.com>
2018-03-26 09:10:39 +0200

    median works

--- src/tpMorphology.cpp
+++ src/tpMorphology.cpp
@@ -9,120 +9,143 @@ using namespace std;
 
 
 /**
-    Compute a median filter of the input float image.
-    The filter window is a square of (2*size+1)*(2*size+1) pixels.
+  Compute a median filter of the input float image.
+  The filter window is a square of (2*size+1)*(2*size+1) pixels.
 
-    Values outside image domain are ignored.
+  Values outside image domain are ignored.
 
-    The median of a list l of n>2 elements is defined as:
-     - l[n/2] if n is odd 
-     - (l[n/2-1]+l[n/2])/2 is n is even 
-*/
+  The median of a list l of n>2 elements is defined as:
+  - l[n/2] if n is odd
+  - (l[n/2-1]+l[n/2])/2 is n is even
+  */
 Mat median(Mat image, int size)
 {
     Mat res = image.clone();
     assert(size>0);
-    /********************************************
-                YOUR CODE HERE
-    *********************************************/
-    
-    /********************************************
-                END OF YOUR CODE
-    *********************************************/
+
+    Rect imageRect = Rect(0, 0, image.cols, image.rows);
+    for (int y = 0; y < image.rows; ++y) {
+        for (int x = 0; x < image.cols; ++x) {
+            Point2i pixel = {y, x};
+            std::vector<float> values;
+
+            for (int i = -size; i <= size; ++i) {
+                for (int j = -size; j <= size; ++j) {
+
+                    Point2i neighbour = {j, i};
+                    neighbour += pixel;
+
+                    if (!neighbour.inside(imageRect)) continue;
+
+                    values.push_back(image.at<float>(neighbour));
+                }
+            }
+            std::sort(values.begin(), values.end());
+
+            int value;
+            if (values.size() % 2 == 0) {
+                value = (values[values.size() / 2 - 1] + values[values.size() / 2]) / 2;
+            } else {
+                value = values[values.size() / 2];
+            }
+            res.at<float>(x, y) = value;
+        }
+    }
+
     return res;
 }
 
 
 /**
-    Compute the erosion of the input float image by the given structuring element.
-    Pixel outside the image are supposed to have value 1.
-*/
+  Compute the erosion of the input float image by the given structuring element.
+  Pixel outside the image are supposed to have value 1.
+  */
 Mat erode(Mat image, Mat structuringElement)
 {
     Mat res = image.clone();
     /********************************************
-                YOUR CODE HERE
-    *********************************************/
-   
+      YOUR CODE HERE
+     *********************************************/
+
     /********************************************
-                END OF YOUR CODE
-    *********************************************/
+      END OF YOUR CODE
+     *********************************************/
     return res;
 }
 
 
 /**
-    Compute the dilation of the input float image by the given structuring element.
-     Pixel outside the image are supposed to have value 0
-*/
+  Compute the dilation of the input float image by the given structuring element.
+  Pixel outside the image are supposed to have value 0
+  */
 Mat dilate(Mat image, Mat structuringElement)
 {
     Mat res = Mat::zeros(1,1,CV_32FC1);
     /********************************************
-                YOUR CODE HERE
-        hint : 1 line of code is enough
-    *********************************************/
-    
+      YOUR CODE HERE
+hint : 1 line of code is enough
+     *********************************************/
+
     /********************************************
-                END OF YOUR CODE
-    *********************************************/
+      END OF YOUR CODE
+     *********************************************/
     return res;
 }
 
 
 /**
-    Compute the opening of the input float image by the given structuring element.
-*/
+  Compute the opening of the input float image by the given structuring element.
+  */
 Mat open(Mat image, Mat structuringElement)
 {
 
     Mat res = Mat::zeros(1,1,CV_32FC1);
     /********************************************
-                YOUR CODE HERE
-        hint : 1 line of code is enough
-    *********************************************/
-    
+      YOUR CODE HERE
+hint : 1 line of code is enough
+     *********************************************/
+
     /********************************************
-                END OF YOUR CODE
-    *********************************************/
+      END OF YOUR CODE
+     *********************************************/
     return res;
 }
 
 
 /**
-    Compute the closing of the input float image by the given structuring element.
-*/
+  Compute the closing of the input float image by the given structuring element.
+  */
 Mat close(Mat image, Mat structuringElement)
 {
 
     Mat res = Mat::zeros(1,1,CV_32FC1);
     /********************************************
-                YOUR CODE HERE
-        hint : 1 line of code is enough
-    *********************************************/
-    
+      YOUR CODE HERE
+hint : 1 line of code is enough
+     *********************************************/
+
     /********************************************
-                END OF YOUR CODE
-    *********************************************/
+      END OF YOUR CODE
+     *********************************************/
     return res;
 }
 
 
 /**
-    Compute the morphological gradient of the input float image by the given structuring element.
-*/
+  Compute the morphological gradient of the input float image by the given structuring element.
+  */
 Mat morphologicalGradient(Mat image, Mat structuringElement)
 {
 
     Mat res = Mat::zeros(1,1,CV_32FC1);
     /********************************************
-                YOUR CODE HERE
-        hint : 1 line of code is enough
-    *********************************************/
-    
+      YOUR CODE HERE
+hint : 1 line of code is enough
+     *********************************************/
+
     /********************************************
-                END OF YOUR CODE
-    *********************************************/
+      END OF YOUR CODE
+     *********************************************/
     return res;
 }
 
